cc8d8e34ed01e168f1ae78d5f23a28a822dbc9c8
APNeto <penaneto@gmail.com>
2022-02-17 19:00:03 -0300

    Initial book api app
    Created initial view with listing
    Created models for book
    Created urls for routing to /books/
    Altered project urls to include books routes
    Altered project settings to include book api app

--- books/book_api/views.py
+++ books/book_api/views.py
@@ -1,3 +1,12 @@
+from django.http import JsonResponse
 from django.shortcuts import render
+from book_api.models import Book
 
 # Create your views here.
+
+def book_list(request):
+    books = Book.objects.all()
+    books_list = list(books.values())
+    return JsonResponse({
+        'books': books_list
+    })
